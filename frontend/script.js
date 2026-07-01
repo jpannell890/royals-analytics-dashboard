deebf1b4ef940b7408eba8445b4a124020b85350
jpannell890 <jwpannell10@gmail.com>
2026-06-30 23:06:18 -0500

    Add standings and schedule sections and update CSS styling

--- frontend/script.js
+++ frontend/script.js
@@ -3,13 +3,67 @@ const BASE_URL = "http://127.0.0.1:8000";
 async function loadRecord() {
     const res = await fetch(`${BASE_URL}/royals/record`);
     const data = await res.json();
-    document.getElementById("record").textContent = JSON.stringify(data, null, 2);
+
+    // Find the Royals inside the giant standings array
+    const royals = data.records[1].teamRecords.find(
+        team => team.team.id === 118
+    );
+
+    document.getElementById("record").innerHTML = `
+        <table>
+            <tr>
+                <th>Wins</th>
+                <th>Losses</th>
+                <th>Win %</th>
+            </tr>
+            <tr>
+                <td>${royals.wins}</td>
+                <td>${royals.losses}</td>
+                <td>${royals.winningPercentage}</td>
+            </tr>
+        </table>
+    `;
 }
 
 async function loadSchedule() {
     const res = await fetch(`${BASE_URL}/royals/schedule`);
     const data = await res.json();
-    document.getElementById("schedule").textContent = JSON.stringify(data, null, 2);
+
+    let html = "<h2>Games</h2><ul>";
+
+    data.dates.forEach(day => {
+
+        if (day.date < "2026-03-27") return;
+
+        day.games.forEach(game => {
+
+            if (game.status.detailedState === "Postponed") return;
+
+            const away = game.teams.away;
+            const home = game.teams.home;
+
+            const royalsIsAway = away.team.id === 118;
+            const royalsIsHome = home.team.id === 118;
+
+            if (royalsIsAway || royalsIsHome) {
+                const opponent = royalsIsAway ? home.team.name : away.team.name;
+                const royalsScore = royalsIsAway ? away.score : home.score;
+                const oppScore = royalsIsAway ? home.score : away.score;
+
+                const result = game.status.detailedState;
+
+                html += `
+                    <li>
+                        ${day.date}: Royals ${royalsScore} vs ${opponent} ${oppScore} (${result})
+                    </li>
+                `;
+            }
+        })
+    })
+
+    html += "</ul>";
+
+    document.getElementById("schedule").innerHTML = html
 }
 
 async function loadPlayers() {
